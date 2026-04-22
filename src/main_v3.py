52ba32cc862f386bb1789761d175b04c56bec88f
david <david.calle@cunef.edu>
2026-04-22 16:54:33 +0200

    añadido de comentarios

--- src/main_v3.py
+++ src/main_v3.py
@@ -1,12 +1,20 @@
 from entrenamiento_v3 import train, evaluate, demo
 
 def main():
+    # entry point for the program
+    # runs training, evaluation and finally the visual demonstration
     print("PRÁCTICA RL - VERSIÓN 3")
     print("Entorno personalizado: DroneSwarm\n")
 
+    #train the agent using Q-learning
     train()
+
+    #evaluation of the policy earned by the episodes
     evaluate()
+
+    #shows visually what the model has learned
     demo()
 
 if __name__ == "__main__":
+    #ensures that main() is executed when the file is run directly
     main()
